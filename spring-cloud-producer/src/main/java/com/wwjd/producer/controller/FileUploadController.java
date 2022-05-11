77c89c77e9d5286d8b6e06caa2259b2697072d5e
guiliu <liugui@zhuofansoft.com>
2022-05-11 20:46:56 +0800

    zuul 网关配置，涉及到文件上传。

---
+++ spring-cloud-producer/src/main/java/com/wwjd/producer/controller/FileUploadController.java
@@ -0,0 +1,31 @@
+/**
+ * @密码学院-尹洪亮(kevin)版权所有,私自转发、售卖、传播将追究法律责任
+ * @作者QQ:1562486028
+ * @技术交流群:660567408 (免费课程源码下载、任何问题的交流)
+ * 
+ * @打开如下连接(最好在手机上)成为我们得推广员,佣金高达30%,支付宝到账结算，期待您的加入。
+ *  https://m.study.163.com/promotion/invite?invitingCode=C8QOKQ
+ */
+package com.wwjd.producer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+@RestController
+public class FileUploadController {
+	
+	@RequestMapping("/upload")
+    public String  fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        String path="D:/"+new Date().getTime()+file.getOriginalFilename();
+        File newFile=new File(path);
+        file.transferTo(newFile);
+        return "success";
+    }
+	
+}
